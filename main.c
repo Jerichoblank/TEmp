c371638345cd6c2f58b594eba82a044c56a6537e
Jerichoblank <89247827+Jerichoblank@users.noreply.github.com>
2021-10-21 15:00:17 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,20 @@ void main()
 {
     int tmp;
 
-    printf("ibutang kung bugnaw o init ang panahon : ");
+    printf("Enter weather temperature : ");
     scanf("%d" ,&tmp);
     if(tmp<0)
-              printf("sobra ka bugnaw na panahon.\n");
+              printf("The weather is too cold.\n");
     else if(tmp<10)
-            printf("grabe ka bugnaw ang panahon.\n");
+            printf("The weather is very cold.\n");
           else if(tmp<20)
-               printf("tugnaw ang panahon.\n");
+               printf("It's cold today.\n");
              else if(tmp<30)
-                   printf("normal na temperatura.\n");
+                   printf("The weather temperature is normal.\n");
                   else if(tmp<40)
-                     printf("init ang panahon.\n");
+                     printf("The weather is hot.\n");
                         else
-                            printf("grabe ka init.\n");
+                            printf("The weather is very hot.\n");
 
 
 
